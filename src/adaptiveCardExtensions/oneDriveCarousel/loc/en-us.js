3aa984c9810386789f0dec8f7e7f15dc21b4e632
guido-zambarda <guido@piasys.it>
2022-03-28 10:12:02 +0200

    Changed quick view button text

--- src/adaptiveCardExtensions/oneDriveCarousel/loc/en-us.js
+++ src/adaptiveCardExtensions/oneDriveCarousel/loc/en-us.js
@@ -9,7 +9,7 @@ define([], function() {
     "SubTitle": "Quick View",
     "Description": "Create your SPFx Adaptive Card Extension solution!",
     "PrimaryText": "SPFx Adaptive Card Extension",
-    "QuickViewButton": "Quick View",
+    "QuickViewButton": "Infos",
     "ErrorMessage": "An error occurred, please try again.",
     "SelectedDriveIdDropdownLabel": "Select the folder that contains the images",
     "TimerMinutesSliderLabel": "Time range in minutes for the carusel to change the selected image",
